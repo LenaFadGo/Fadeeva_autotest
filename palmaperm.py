9f0d10fa16c8e19120f5281707e773bdcf3e4f06
RERIK <glebkrot200800@gmail.com>
2026-03-16 21:06:19 +0500

    Работа с Selenium

---
+++ palmaperm.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.chrome.options import Options
+
+# options = Options()
+# options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_argument("--disable-site-isolation-trials")
+# options.add_argument("--ignore-certificate-errors")
+# options.add_argument("--allow-insecure-localhost")
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 10)
+
+# driver.get ("https://palmaperm.ru/")
+# element = driver.find_element(By.XPATH, "//span[text()='Корзина']")
+# element.click()
+
+def test_open_palmaperm():
+    driver.get("https://palmaperm.ru/")
+    accept_button = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[text()='Корзина']")))
+    accept_button.click()
+
